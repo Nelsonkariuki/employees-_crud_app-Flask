8e290dcd8ad675be53664905829e0a4f7b5a4598
Nelsonkariuki <nelsonwainaina99@gmail.com>
2022-07-02 01:02:14 +0300

    crud

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from models import db
-from flask import Flask
+from models import EmployeeModel, db
+from flask import Flask, redirect, render_template, request
 app =Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -8,5 +8,25 @@ db.init_app(app)
 @app.before_first_request
 def create_table():
     db.create_all()
- 
+
 app.run(host='localhost', port=5000)
+
+@app.route('/data/create' , methods = ['GET','POST'])
+
+def create():
+    if request.method == 'GET':
+        return render_template('createpage.html')
+ 
+    if request.method == 'POST':
+        employee_id = request.form['employee_id']
+        name = request.form['name']
+        age = request.form['age']
+        position = request.form['position']
+        employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position = position)
+        db.session.add(employee)
+        db.session.commit()
+        return redirect('/data')
+@app.route('/data')
+def RetrieveDataList():
+    employees = EmployeeModel.query.all()
+    return render_template('datalist.html',employees = employees)
