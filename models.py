168597b9375b195e72161259c5693c016aa4d287
Nelsonkariuki <nelsonwainaina99@gmail.com>
2022-07-02 00:18:19 +0300

    database completion

--- models.py
+++ models.py
@@ -10,4 +10,13 @@ class EmployeeModel(db.model):
     age=db.Column(db.Integer())
     position = db.Column(db.String(80))
     
+    def __init__(self, employee_id,name,age,position):
+        self.employee_id = employee_id
+        self.name = name
+        self.age = age
+        self.position = position
+ 
+    def __repr__(self):
+        return f"{self.name}:{self.employee_id}"
+    
     
