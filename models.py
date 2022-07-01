ceab8335c65fabd2bf23ec6649a794047bf46a8b
Nelsonkariuki <nelsonwainaina99@gmail.com>
2022-07-01 22:35:38 +0300

    db column

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
+from enum import unique
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 class EmployeeModel(db.model):
     __tablename__ = "table"
     id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.Integer)
+    employee_id = db.Column(db.Integer(),unique = True)
     
