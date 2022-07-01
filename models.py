3dd9ff19b5436efc5b282ec739aa241b2d10960b
Nelsonkariuki <nelsonwainaina99@gmail.com>
2022-07-01 22:28:40 +0300

    Models

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 class EmployeeModel(db.model):
+    __tablename__ = "table"
+    id = db.Column(db.Integer, primary_key=True)
+    empl
+    
