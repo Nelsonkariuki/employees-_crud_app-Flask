053cde7ab8c9247108b1e2448b192dfecd9189db
Nelsonkariuki <nelsonwainaina99@gmail.com>
2022-07-02 00:57:49 +0300

    crud application

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from django import db
+from models import db
 from flask import Flask
 app =Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.db'
--- models.py
+++ models.py
@@ -5,9 +5,9 @@ class EmployeeModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     employee_id = db.Column(db.Integer(),unique = True)
     name = db.Column(db.String())
-    age=db.Column(db.Integer())
+    age = db.Column(db.Integer())
     position = db.Column(db.String(80))
-    
+ 
     def __init__(self, employee_id,name,age,position):
         self.employee_id = employee_id
         self.name = name
@@ -16,5 +16,3 @@ class EmployeeModel(db.Model):
  
     def __repr__(self):
         return f"{self.name}:{self.employee_id}"
-    
-    
